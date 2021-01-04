833c2062788229af4a04d55f666bdc15d1ae4131
GunjanBhat1a <76939553+GunjanBhat1a@users.noreply.github.com>
2021-01-04 19:51:44 +0530

    Update BallMovement.java

--- OOPBasedBallTask/src/main/java/BallMovement.java
+++ OOPBasedBallTask/src/main/java/BallMovement.java
@@ -1,21 +1,18 @@
 import processing.core.PApplet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BallMovement extends PApplet {
 
     public static final int WIDTH = 640;
     public static final int HEIGHT = 600;
     public static final int Y_SCALE = HEIGHT / 5;
-    public static final int SPEED_FACTOR1 = 1;
-    public static final int SPEED_FACTOR2 = 2;
-    public static final int SPEED_FACTOR3 = 3;
-    public static final int SPEED_FACTOR4 = 4;
+    public static final int CAPACITY = 4;
 
-    Ball ball1 = null;
-    Ball ball2 = null;
-    Ball ball3 = null;
-    Ball ball4 = null;
+    List<Ball> balls = new ArrayList<>(CAPACITY);
 
     public static void main(String[] args) {
+
         PApplet.main("BallMovement", args);
 
     }
@@ -23,29 +20,30 @@ public class BallMovement extends PApplet {
     @Override
     public void setup() {
 
-        ball1 = new Ball(0, Y_SCALE * SPEED_FACTOR1, SPEED_FACTOR1);
-        ball2 = new Ball(0, Y_SCALE * SPEED_FACTOR2, SPEED_FACTOR2);
-        ball3 = new Ball(0, Y_SCALE * SPEED_FACTOR3, SPEED_FACTOR3);
-        ball4 = new Ball(0, Y_SCALE * SPEED_FACTOR4, SPEED_FACTOR4);
-
+        for (int i = 1; i <= CAPACITY; i++) {
+            balls.add(new Ball(0, getPositionY(i), i));
+        }
     }
+
+    private int getPositionY(int i) {
+        return Y_SCALE * i;
+    }
+
 
     @Override
     public void settings() {
-        super.settings();
         size(WIDTH, HEIGHT);
     }
 
     @Override
     public void draw() {
-        showMovingBalls(ball1);
-        showMovingBalls(ball2);
-        showMovingBalls(ball3);
-        showMovingBalls(ball4);
+
+        for (Ball ball : balls) {
+            showMovingBalls(ball);
+        }
     }
 
     private void showMovingBalls(Ball ball) {
-        ellipse(ball.getPositionX(),ball.getPositionY(), Ball.DIAMETER, Ball.DIAMETER);
-
+        ellipse(ball.getPositionX(), ball.getPositionY(), Ball.DIAMETER, Ball.DIAMETER);
     }
 }
